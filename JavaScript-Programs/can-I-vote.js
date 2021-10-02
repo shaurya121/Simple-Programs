baf30cb3816e517fee2e50e99ca420110f1968be
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-10-02 20:25:59 +0545

    Create can-I-vote.js

---
+++ JavaScript-Programs/can-I-vote.js
@@ -0,0 +1,9 @@
+let age = 17;
+
+if (age >= 18){
+	console.log('You are eligible to vote');
+} else if (age < 18) {
+	console.log('You are not eligible to vote');
+} else {
+  console.log('ERROR 404');
+}
