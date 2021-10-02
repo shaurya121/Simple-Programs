cdd8efaed8c495673c3281fb8c5664196079a1a1
Aayush_Dhakal <therespawner69@gmail.com>
2021-10-02 08:48:27 +0545

    Adding percentage calculation

--- C-Programs/Calculator.c
+++ C-Programs/Calculator.c
@@ -2,7 +2,7 @@
 int main() {
   char op;
   double first, second;
-  printf("Enter an operator (+, -, *, /): ");
+  printf("Enter an operator (+, -, *, /, %%) : " );
   scanf("%c", &op);
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
@@ -19,6 +19,9 @@ int main() {
       break;
     case '/':
       printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    case '%':
+      printf("(%.1lf /100)* %.1lf = %.1lf", first, second, (first/100)*second);
       break;
     default:
       printf("Error! operator is not correct");
