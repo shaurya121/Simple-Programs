e3f8d00527164f894473da92816bd5483e60e005
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-10-03 05:57:12 +0545

    Create can-I-use-discord.js

---
+++ JavaScript-Programs/can-I-use-discord.js
@@ -0,0 +1,9 @@
+let age = prompt("What is your age?");
+
+let leftYears = 13 - age;
+
+if (age >= 13) {
+  console.log("You can use discord");
+} else {
+  console.log("You need to wait " + leftYears + " years to use discord.");
+}
