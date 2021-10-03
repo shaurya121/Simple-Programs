2c2bc9b9bea41a1c5210229f0ccc96b0489e43f6
Vinicius Sales <viniciusc.doom@gmail.com>
2021-10-03 11:34:00 -0300

    Added Rust basic info

---
+++ Rust-Programs/rust_basics/main.rs
@@ -0,0 +1,23 @@
+mod print;
+mod var;
+mod types;
+mod strings;
+mod tuples;
+mod arrays;
+mod vectors;
+mod conditionals;
+mod loops;
+mod functions;
+
+fn main() {
+    print::run();
+    var::run();
+    types::run();
+    strings::run();
+    tuples::run();
+    arrays::run();
+    vectors::run();
+    conditionals::run();
+    loops::run();
+    functions::run();
+}
