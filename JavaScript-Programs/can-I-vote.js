3bc6f407e6a9fb346a95f25657dbc769db568505
TheCoolDeveloper <76424367+TheCoolGDev@users.noreply.github.com>
2021-10-03 05:58:17 +0545

    Delete can-I-vote.js

--- JavaScript-Programs/can-I-vote.js
+++
@@ -1,9 +0,0 @@
-let age = 17;
-
-if (age >= 18){
-	console.log('You are eligible to vote');
-} else if (age < 18) {
-	console.log('You are not eligible to vote');
-} else {
-  console.log('ERROR 404');
-}
